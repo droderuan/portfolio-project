865bf087da91a4463ab7a18887f1e382770a605d
Ruan <droderuan@hotmail.com>
2019-08-13 23:30:52 -0300

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,4 @@ class Blog(models.Model):
     title = models.CharField(max_length=30)
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.TextField()
--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image_height = 255
+    image = models.ImageField(upload_to='images/', height_field='image_height')
     summary = models.CharField(max_length=200)
