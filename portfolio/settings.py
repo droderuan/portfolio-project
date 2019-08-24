53abe0e5ded2d89d3f86b4a1bd2c403498c21df2
Ruan <droderuan@hotmail.com>
2019-08-24 01:18:31 -0300

    added requirements file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,10 +134,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-
 try:
     from local_setting.py import *
 except ImportError:
